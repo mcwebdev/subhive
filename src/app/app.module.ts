bcfa4208ccaaec7c739caf147f2054ca8459cbdc
Matt Charlton <mattcharlton33@gmail.com>
2022-10-31 19:50:02 -0700

    authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/login/dashboard/dashboard.component';
+import { SignInComponent } from './components/login/sign-in/sign-in.component';
+import { SignUpComponent } from './components/login/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/login/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/login/verify-email/verify-email.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Auth service
+import { AuthService } from "./shared/services/auth.service";
+
+// Firebase services + environment module
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCfM3WZgs2kbGrcopH4e0R6ugrw11t2nVY",
+    authDomain: "subhive-60d13.firebaseapp.com",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "subhive-60d13",
+    storageBucket: "subhive-60d13.appspot.com",
+    messagingSenderId: "5198912773",
+    appId: "1:5198912773:web:388bccf5d9ff48df9bab6c",
+    measurementId: "G-S59H7J78KC"
+  }
 };
 
 /*
